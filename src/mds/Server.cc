45a241ebbb3b1aaa166d1e4fd1970a46c5668931
Li Sheng <lishenggle@gmail.com>
2026-09-18 13:45:31 +0800

    mds: don't end a snapdiff dirfrag before all its entries are sent
    build_snap_diff() reports completion based on the iterator position,
    which may already be at end() when the last entry fails to fit. It
    also ignores a pending deletion that fails to fit after the loop.
    The client then sees CEPH_READDIR_FRAG_END and skips the missing entry.
    
    Return false whenever an entry does not fit, and report completion
    only after all entries, including a pending deletion, are emitted.
    
    Fixes: https://tracker.ceph.com/issues/80639
    Signed-off-by: Li Sheng <lishenggle@gmail.com>

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -12660,7 +12660,7 @@ bool Server::build_snap_diff(
       // hence need to insert the previous entry if any immediately.
       if (before.dn) {
 	if (!insert_deleted(before)) {
-	  break;
+	  return false;
 	}
       }
 
@@ -12677,7 +12677,7 @@ bool Server::build_snap_diff(
       }
       bool r = add_result_cb(dn, in, exists);
       if (!r) {
-	break;
+	return false;
       }
     } else {
       if (snapid_prev >= dn->first && snapid <= dn->last) {
@@ -12689,7 +12689,7 @@ bool Server::build_snap_diff(
         bool locked = false;
         if (snapflush_pending(in)) {
           if (before.valid() && !insert_deleted(before))
-            break;
+            return false;
           if (!rdlock_file_start(in))
             return false;
           locked = true;
@@ -12738,7 +12738,7 @@ bool Server::build_snap_diff(
 
         // Preserve hash/name ordering if a deleted entry is pending.
         if (before.valid() && !insert_deleted(before))
-          break;
+          return false;
 
         if (attrs_known) {
           dout(20) << __func__
@@ -12756,7 +12756,7 @@ bool Server::build_snap_diff(
         }
 
         if (!add_result_cb(dn, in, true))
-          break;
+          return false;
         continue;
       } else if (snapid_prev < dn->first && snapid > dn->last) {
 	dout(20) << __func__ << " skipping inner modification " << dn->get_name() << " "
@@ -12765,7 +12765,7 @@ bool Server::build_snap_diff(
       }
       if (before.valid() && before.dn->get_name() != dn->get_name()) {
         if (!insert_deleted(before)) {
-          break;
+          return false;
         }
         before.reset();
       }
@@ -12781,7 +12781,7 @@ bool Server::build_snap_diff(
 		     << dn->first << "/" << dn->last
 		     << dendl;
 	    if (!insert_deleted(before)) {
-	      break;
+	      return false;
 	    }
 	    before.reset();
 	  } else {
@@ -12830,12 +12830,12 @@ bool Server::build_snap_diff(
 	ceph_assert(snapid >= dn->first && snapid <= dn->last);
       }
       if (!add_result_cb(dn, in, true)) {
-	break;
+	return false;
       }
     }
   }
-  if (before.dn) {
-    insert_deleted(before);
+  if (before.dn && !insert_deleted(before)) {
+    return false;
   }
-  return it == dir->end();
+  return true;
 }
